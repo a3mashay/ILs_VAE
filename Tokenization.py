e4016b5dcc8697f1b798f516a9fd54e66cc85ff5
a3mashay <59024209+a3mashay@users.noreply.github.com>
2025-02-19 09:53:41 -0500

    Add files via upload

--- Tokenization.py
+++ Tokenization.py
@@ -1,36 +1,11 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Feb 19 09:42:26 2025
-
-@author: mashi
-"""
-
 def custom_tokenize_smiles(smiles_list):
     """
-    Custom tokenization for SMILES strings to better capture chemical structures.
+    Custom tokenization for SMILES strings.
     """
     tokenized_smiles = []
-    special_tokens = {'C', '(', ')', 'F', 'c', 'O', '[', ']', '=', '1', 'N', '-', '+', 'n', 'S', 'B', 'H',
-                        '@', '2', 'r', '#', 'P', 'l', '3', 'I', '/', 'i', 'A', 's', 'o', '4', 'b',
-                      'CC', ')(', 'F)', ')C', '(=', '(F', 'C(', 'O)', '=O', '(C', 'cc', 'C)', '+]', '-]',
-                       '[N', ')F', 'c1', 'C[', '](', ')[', '1c', '=C', 'S(', '1)', ']1', '(c', 'C1', '[n',
-                       'n+', 'N+', 'OC', 'C=', '[O', 'O-', 'FC', 'N-', '[B', 'CO', '1C', ']S', 'Br', '(S',
-                       'n(', 'r-', 'cn', 'c(', '[C', '])', 'NH', 'H]', '@H', 'n1', 'C@', '1=', 'c[', '#N',
-                       'c2', '([', ')O', '[P', ']C', 'Cc', 'Cn', 'Cl', ')c', '1(', 'C#', 'N(', 'B-', '@@',
-                       'F[', 'N)', 'H+', 'CN', ')N', '(O', 'P+', 'NC', '2c', '=N', '=[', '2)', ')=', '(#',
-                       'P-', 'H2', '2+', 'N1', '#C', 'C2', 'l-', '(N', 'O=', ']=', 'N=', '1n', 'l)', '2C',
-                       '[S', ')S', 'N#', 'H3', '3+', 'O[', '1N', '2=', '=1', 'OS', 'P(', '1[', 'CS', 'nc',
-                       '(n', '[I', 'I-', 'N[', 'lC', 'r)', 'SC', 'C-', 'Oc', ')B', ']c', ')n', '12', 'S+',
-                       'FS', 'N2', 'N@', 'l(', '@+', 'nH', 'H-', '1O', 'S-', 'CH', 'C3', 'NN', '/C', 'O1',
-                       ']2', 'nn', '2n', 'C/', 'Si', 'i]', '(B', 'OP', '[A', 'rC', 'Al', ']P', 'c3', 'n[',
-                       'lc', '3C', 'Sc', 'CP', '][', 'n2', '3(', '1S', 'cs', 'CB', 'O2', 'Nc', '1F', 'On',
-                       'I)', '2(', '=c', 'O(', '3c', 'rc', 'S)', ')/', 'Nn', 'sc', '3)', ']n', '@]', '1I',
-                       'S1', '(P', 'PO', 'r[', ']N', '2[', '-c', '=S', ')P', '/[', 'n-', ')-', '2N', '=P',
-                       'P[', 'P@', '[H', 'PH', 'ON', 's1', 'S@', '/N', '1B', ')I', 'S/', ']/', ']O', 's2',
-                       '(I', 'OH', '2-', '(o', 'o2', 'NO', 'co', 'o1', 'Fc', 'c/', '/1', '1s', '(/', 'NS',
-                       '3B', '2B', 'H4', '4+', 'P1', 'C+', 'I[', ']I', ']B', 'IC', '1o', 'oc',
-                       ')s', '3n', '/c', 'lS', '1-', '/B', 'Sn', '3=', 'As', 's-', '2F', 'l[', 'Sb', 'b-',
-                       'SS', '[F', 'F-'}
+    special_tokens = {
+        #must be the elements and characters from periodic table
+        }
 
     for smiles in smiles_list:
         i = 0
@@ -49,3 +24,30 @@ def custom_tokenize_smiles(smiles_list):
                 i += 1
         tokenized_smiles.append(tokens)
     return tokenized_smiles
+
+def calculate_token_variance(tokenized_smiles):
+    """
+    Calculate the variance of each token's across the dataset.
+    """
+    # Flatten the list of tokens to get overall frequency
+    all_tokens = [token for sublist in tokenized_smiles for token in sublist]
+    token_counts = Counter(all_tokens)
+
+    # Calculate frequencies
+    num_samples = len(tokenized_smiles)
+    token_frequencies = {token: count / num_samples for token, count in token_counts.items()}
+
+    # Calculate variance of token presence across samples
+    variances = {}
+    for token in token_counts.keys():
+        presence = np.array([token in sample for sample in tokenized_smiles])
+        variances[token] = np.var(presence)
+
+    return variances
+
+# Select tokens based on variance threshold
+def select_informative_tokens(variances, threshold=0.01):
+    """
+    Select tokens with a variance above a certain threshold.
+    """
+    return [token for token, var in variances.items() if var > threshold]

42e63273618fa4aff7ca96711b20e5bb27c51c77
Alireza Mashayekhi <59024209+a3mashay@users.noreply.github.com>
2025-06-16 14:22:12 -0400

    Update SMILES-Based Dataset Preparation.py

--- SMILES-Based Dataset Preparation.py
+++ SMILES-Based Dataset Preparation.py
@@ -19,22 +19,3 @@ def smiles_to_features(smiles, nBits=2048):
     else:
         return np.zeros((nBits,))
 
-def load_data_with_smiles(file_path, target_columns):
-    data = pd.read_excel(file_path)
-    all_smiles = []  # List to hold all SMILES, including augmented ones
-    features = []
-    targets = {col: [] for col in target_columns}  # Initialize a dictionary for each target
-
-    for _, row in data.iterrows():
-        original_smiles = row['Cation'] + '.' + row['Anion']
-        augmented_smiles = augment_smiles(original_smiles, num_augmentations=200)
-        for smiles in augmented_smiles:
-            all_smiles.append(smiles)  # Append each augmented SMILES
-            features.append(smiles_to_features(smiles))
-            for col in target_columns:
-                targets[col].append(row[col])  # Append target for each augmented SMILES
-
-    features = np.array(features)
-    # Convert targets into a format suitable for training
-    targets_array = np.column_stack([targets[col] for col in target_columns])
-    return all_smiles, features, targets_array
